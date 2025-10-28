71ba2175c3fed42fe7857c9dda4b38435f5bf6b8
bhupengit <bhupenjoshi07@gmail.com>
2025-10-28 21:49:07 +0530

    Displaying session transcript

--- components/screens/SummaryScreen.tsx
+++ components/screens/SummaryScreen.tsx
@@ -1,13 +1,14 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Gradient } from '../gradient'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { ConversationResponse } from '@/utils/types';
+import Button from '../Button';
 
 export default function SummaryScreen() {
     const { conversationId } = useLocalSearchParams();
     const [conversation, setConversation] = useState<ConversationResponse>()
-
+    const router = useRouter()
     useEffect(() => {
         getSummary()
     }, [])
@@ -27,8 +28,64 @@ export default function SummaryScreen() {
                 contentInsetAdjustmentBehavior='automatic'
                 contentContainerStyle={{ paddingHorizontal: 16 }}
             >
-                <Text>{conversation?.agent_id}</Text>
+                {conversation?.status !== "done" && (
+                    <View style={{ gap: 16, paddingBottom: 16}}>
+                        <Text style={styles.title}>We are processing your call...</Text>
+                        <Text style={styles.subtitle}>This may take a few minutes.</Text>
+                        <Text style={styles.subtitle}>
+                            Current status: {conversation?.status}
+                        </Text>
+                        <Button onPress={getSummary}>Refresh</Button>
+                    </View>
+                )}
+                {conversation?.status === "done" &&(
+                    <View style={{ gap: 16, paddingBottom: 16}}>
+                        <Text style={styles.caption}>{conversationId}</Text>
+                        <Text style={styles.title}>
+                            {conversation?.analysis?.call_summary_title}
+                        </Text>
+                        <Text style={styles.subtitle}>
+                            {conversation?.analysis?.transcript_summary.trim()}
+                        </Text>
+
+                        <Text style={styles.title}>Stats</Text>
+                        <Text style={styles.subtitle}>
+                            {conversation?.metadata?.call_duration_secs} seconds
+                        </Text>
+                        <Text style={styles.subtitle}>
+                            {conversation?.metadata?.cost} tokens
+                        </Text>
+                        <Text style={styles.subtitle}>
+                            {new Date(
+                                conversation?.metadata.start_time_unix_secs! * 1000
+                            ).toLocaleString()}
+                        </Text>
+
+                        <Text style={styles.title}>Transcript</Text>
+                        <Text style={styles.subtitle}>
+                            {conversation?.transcript.map((t) => t.message).join("\n")}
+                        </Text>
+                    </View>
+                )}
+                <View style={{ alignItems: "center"}} >
+
+                    <Button onPress={() => router.dismissAll()}>Save and continue</Button>
+                </View>
             </ScrollView>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    subtitle:{
+        fontSize: 16
+    },
+    caption:{
+        fontSize: 12,
+        color: "gray",
+    }
+});
